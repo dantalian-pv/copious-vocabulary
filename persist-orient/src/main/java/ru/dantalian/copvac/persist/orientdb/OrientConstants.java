ed1ba260d34760c33b4a9d70fb7008416b6195de
dantalain-pv <dantalian.pv@gmail.com>
2018-11-28 11:48:38 +0100

    Remove orientdb
    Signed-off-by: dantalain-pv <dantalian.pv@gmail.com>

--- persist-orient/src/main/java/ru/dantalian/copvac/persist/orientdb/OrientConstants.java
+++
@@ -1,9 +0,0 @@
-package ru.dantalian.copvac.persist.orientdb;
-
-public interface OrientConstants {
-
-	public static final String CONFIG_DIR = "configDir";
-
-	public static final String DATA_DIR = "dataDir";
-
-}
