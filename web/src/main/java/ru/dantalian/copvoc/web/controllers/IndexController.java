8e2e1bfdaa12a8be010fb714dd6cc5a045417fd6
dantalain-pv <dantalian.pv@gmail.com>
2018-12-10 13:17:59 +0100

    Add white list form pages. Add error pages templates
    Signed-off-by: dantalain-pv <dantalian.pv@gmail.com>

--- web/src/main/java/ru/dantalian/copvoc/web/controllers/IndexController.java
+++ web/src/main/java/ru/dantalian/copvoc/web/controllers/IndexController.java
@@ -1,26 +1,37 @@
 package ru.dantalian.copvoc.web.controllers;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+	private final Set<String> whiteList = new HashSet<>(Arrays.asList("login", "main"));
+
 	@RequestMapping("/")
-  String index(final Principal principal) {
-		if (principal == null) {
+	String index(final Principal aPrincipal, final Model aModel) {
+		if (aPrincipal == null) {
 			return "redirect:/page/login";
 		} else {
-      return "index";
+			aModel.addAttribute("tpl", "main");
+			return "frame";
 		}
-  }
+	}
 
 	@RequestMapping("/page/{page}")
-  String login(@PathVariable("page") final String aPage) {
-      return aPage;
-  }
+	String login(@PathVariable("page") final String aPage, final Model aModel) {
+		if (!whiteList.contains(aPage)) {
+			throw new PageNotFoundException();
+		}
+		aModel.addAttribute("tpl", aPage);
+		return "frame";
+	}
 
 }
