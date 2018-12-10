642f780023fc880a7c484f781be4be913e54aadd
dantalain-pv <dantalian.pv@gmail.com>
2018-12-10 11:27:53 +0100

    Add production profile. Add https
    Signed-off-by: dantalain-pv <dantalian.pv@gmail.com>

--- web/src/main/java/ru/dantalian/copvoc/web/controllers/IndexController.java
+++ web/src/main/java/ru/dantalian/copvoc/web/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package ru.dantalian.copvoc.web.controllers;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping("/")
-  String index(){
+  String index(final Principal principal) {
+		if (principal == null) {
+			return "redirect:/page/login";
+		} else {
       return "index";
+		}
   }
 
 	@RequestMapping("/page/{page}")
-  String login(@PathVariable("page") final String aPage){
+  String login(@PathVariable("page") final String aPage) {
       return aPage;
   }
 
