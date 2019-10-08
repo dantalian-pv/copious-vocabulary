4bcaeabdc682c8e75051264da0e003a39bcd3e1f
dantalain-pv <dantalian.pv@gmail.com>
2019-10-08 14:19:08 +0200

    Finish training immediately
    Signed-off-by: dantalain-pv <dantalian.pv@gmail.com>

--- web/src/main/resources/public/js/tpl/training.js
+++ web/src/main/resources/public/js/tpl/training.js
@@ -1,22 +1,22 @@
 "use strict";
 
 (function ($) {
-  $.fn.serializeFormJSON = function () {
+	$.fn.serializeFormJSON = function () {
 
-      var o = {};
-      var a = this.serializeArray();
-      $.each(a, function () {
-          if (o[this.name]) {
-              if (!o[this.name].push) {
-                  o[this.name] = [o[this.name]];
-              }
-              o[this.name].push(this.value || '');
-          } else {
-              o[this.name] = this.value || '';
-          }
-      });
-      return o;
-  };
+			var o = {};
+			var a = this.serializeArray();
+			$.each(a, function () {
+					if (o[this.name]) {
+							if (!o[this.name].push) {
+									o[this.name] = [o[this.name]];
+							}
+							o[this.name].push(this.value || '');
+					} else {
+							o[this.name] = this.value || '';
+					}
+			});
+			return o;
+	};
 })(jQuery);
 
 $(document).ready(
@@ -28,9 +28,9 @@ $(document).ready(
 
 			function Item(data) {
 				for (var key in data) {
-			    if (data.hasOwnProperty(key)) {
-			    	this[key] = ko.observable(data[key]);
-			    }
+					if (data.hasOwnProperty(key)) {
+						this[key] = ko.observable(data[key]);
+					}
 				}
 			}
 
@@ -45,21 +45,21 @@ $(document).ready(
 			function ItemGroupListViewModel() {
 				// Data
 				var self = this;
-				
+
 				self.item = ko.observable();
 
 				self.isShowAnswer = ko.observable(false);
-				
+
 				self.showAnswer = function() {
-					
+
 				}
 
 				self.errorHeader = ko.observable();
 				self.errorMessage = ko.observable();
-				
+
 				self.cardIndx = ko.observable(1);
 				self.cardSize = ko.observable(document.cardsSize);
-				
+
 				self.showNext = function(aValidated) {
 					$.getJSON('/v1/api/train/' + document.trainingId + '/' + self.item().id() + '/_next' + (aValidated === true? '?validated=true': ''), function(allData) {
 						if (allData.id) {
@@ -72,57 +72,61 @@ $(document).ready(
 						}
 					}).fail(xhrErrorHandler);
 				};
-				
+
+				self.finishTraining = function() {
+					window.location.href = '/training/' + document.trainingId + '/result';
+				};
+
 				self.errorHeader = ko.observable();
 				self.errorMessage = ko.observable();
-				
+
 				self.errors = ko.observable(0);
 
 				$('#check_card_form').submit(function(evt) {
 					evt.preventDefault();
 					self.errorHeader('');
-    			self.errorMessage('');
-    			$.ajax({
-    	           url: '/v1/api/train/' + document.trainingId + '/' + self.item().id(),
-    	           type : "POST",
-    	           headers : csrf,
-    	           dataType : 'json',
-	   						 statusCode: {
-	  								401: function() {
-	  									window.location.replace(window.location.href);
-	  								}
-	  						 },
-    	           contentType: "application/json; charset=utf-8",
-    	           data : JSON.stringify($("#check_card_form").serializeFormJSON()),
-    	           success : function(result) {
-    	          	 if (result.valid) {
-    	          		 self.errors(0);
-    	          		 self.showNext(true);
-    	          	 } else {
-    	          		 self.errorHeader('Wrong answer');
-    	          		 self.errorMessage(result.message);
-    	          		 if (self.errors() > 2) {
-    	          			 self.errors(0);
-    	          			 self.showNext(true);
-    	          		 } else {
-    	          			 self.errors(self.errors() + 1);
-    	          		 }
-    	          	 }
-    	           },
-    	           error: function(xhr, resp, text) {
-    	        	   if (!xhr.responseJSON) {
-    	        	  	 self.errorHeader(xhr.status);
-    	        	  	 self.errorMessage(xhr.statusText);
-    	        	   } else {
-    	        	  	 var e = xhr.responseJSON;
-    	        	  	 self.errorHeader('Failed to Check');
-    	        	  	 self.errorMessage(e.message);
-    	        	   }
-    	           }
-    	        });
-			    return false;
+					self.errorMessage('');
+					$.ajax({
+								 url: '/v1/api/train/' + document.trainingId + '/' + self.item().id(),
+								 type : "POST",
+								 headers : csrf,
+								 dataType : 'json',
+									statusCode: {
+										401: function() {
+											window.location.replace(window.location.href);
+										}
+								 },
+								 contentType: "application/json; charset=utf-8",
+								 data : JSON.stringify($("#check_card_form").serializeFormJSON()),
+								 success : function(result) {
+									 if (result.valid) {
+										 self.errors(0);
+										 self.showNext(true);
+									 } else {
+										 self.errorHeader('Wrong answer');
+										 self.errorMessage(result.message);
+										 if (self.errors() > 2) {
+											 self.errors(0);
+											 self.showNext(true);
+										 } else {
+											 self.errors(self.errors() + 1);
+										 }
+									 }
+								 },
+								 error: function(xhr, resp, text) {
+									 if (!xhr.responseJSON) {
+										 self.errorHeader(xhr.status);
+										 self.errorMessage(xhr.statusText);
+									 } else {
+										 var e = xhr.responseJSON;
+										 self.errorHeader('Failed to Check');
+										 self.errorMessage(e.message);
+									 }
+								 }
+							});
+					return false;
 				});
-				
+
 				self.convertItem = function(data) {
 					var flatItem = {};
 					flatItem['id'] = data.id;
@@ -132,7 +136,7 @@ $(document).ready(
 					}
 					return flatItem;
 				};
-				
+
 				self.updateData = function(cardId) {
 					// Load initial state from server
 					if (!document.trainingId) {
